5febe595054ad29f7807520b88238d90f74ae8e5
Priyanka <wadekarp384@gmail.com>
2026-05-05 20:22:29 +0530

    Enable CORS

--- smart-parking-backend/server.js
+++ smart-parking-backend/server.js
@@ -17,7 +17,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: "https://smart-parking-availability-platform.vercel.app/"
+  origin: "*"
 }));
 
 app.use(express.json());
@@ -31,6 +31,7 @@ app.use("/api/auth", authRoutes);
 
 // ✅ FIX: Render requires dynamic PORT
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
